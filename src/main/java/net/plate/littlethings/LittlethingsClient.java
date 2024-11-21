48baf48c633408ed19956edd8c9b3ab20dafccfe
7mwang <matthewxpwang@gmail.com>
2024-11-20 21:53:28 -0800

    Initial commit

---
+++ src/main/java/net/plate/littlethings/LittlethingsClient.java
@@ -0,0 +1,11 @@
+package net.plate.littlethings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LittlethingsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
